2a80d28fd65e948868cd037368a1b1307785eb2e
HJyeoh <jinghaoyeoh157@gmail.com>
2024-12-25 18:38:17 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,15 @@ import Cart from './Pages/Cart';
 import Product from './Pages/Product';
 import Home from './Pages/Home';
 import Footer from './Components/Footer/Footer';
+import boy_clothes_banner from './Components/Assets/boy_clothes_banner.png'
+import girl_clothes_banner from './Components/Assets/girl_clothes_banner.png'
+import newborn_clothes_banner from './Components/Assets/newborn_clothes_banner.png'
+import maternity_clothes_banner from './Components/Assets/maternity_clothes_banner.png'
+import feeding_banner from './Components/Assets/feeding_banner.png'
+import maternity_care_banner from './Components/Assets/maternity_care_banner.png'
+import furniture_banner from './Components/Assets/furniture_banner.png'
+import toys_banner from './Components/Assets/toys_banner.png'
+import shoes_banner from './Components/Assets/shoes_banner.png'
 
 function App() {
   return (
@@ -15,15 +24,15 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/boy_clothes" element={<ShopCategory category="boy_clothes" />} />
-          <Route path="/girl_clothes" element={<ShopCategory category="girl_clothes" />} />
-          <Route path="/newborn_clothes" element={<ShopCategory category="newborn_clothes" />} />
-          <Route path="/shoes" element={<ShopCategory category="shoes" />} />
-          <Route path="/toys" element={<ShopCategory category="toys" />} />
-          <Route path="/feeding" element={<ShopCategory category="feeding" />} />
-          <Route path="/maternity_care" element={<ShopCategory category="maternity_care" />} />
-          <Route path="/furniture" element={<ShopCategory category="furniture" />} />
-          <Route path="/maternity_clothes" element={<ShopCategory category="maternity_clothes" />} />
+          <Route path="/boy_clothes" element={<ShopCategory banner={boy_clothes_banner} category="boy_clothes" />} />
+          <Route path="/girl_clothes" element={<ShopCategory banner={girl_clothes_banner} category="girl_clothes" />} />
+          <Route path="/newborn_clothes" element={<ShopCategory banner={newborn_clothes_banner} category="newborn_clothes" />} />
+          <Route path="/shoes" element={<ShopCategory banner={shoes_banner} category="shoes" />} />
+          <Route path="/toys" element={<ShopCategory banner={toys_banner} category="toys" />} />
+          <Route path="/feeding" element={<ShopCategory banner={feeding_banner} category="feeding" />} />
+          <Route path="/maternity_care" element={<ShopCategory banner={maternity_care_banner} category="maternity_care" />} />
+          <Route path="/furniture" element={<ShopCategory banner={furniture_banner} category="furniture" />} />
+          <Route path="/maternity_clothes" element={<ShopCategory banner={maternity_clothes_banner} category="maternity_clothes" />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
