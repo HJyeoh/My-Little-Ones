7bf3c333c60202de5d6f3d43af211f0e9685fa26
HJyeoh <jinghaoyeoh157@gmail.com>
2025-01-11 01:13:21 +0800

    PW/payment page , HJ/Review Box

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -40,7 +40,7 @@ const DescriptionBox = () => {
       </div>
       <div className="descriptionbox-description">
         <p>
-          My Little Ones is your one-stop destination for high-quality baby and mommy essentials, designed to make parenting joyful and stress-free. From cosy baby clothes and safe feeding products to stylish maternity wear and playful toys, we offer a carefully curated selection of trusted items to meet your family’s needs. With a focus on comfort, safety, and affordability, My Little Ones is dedicated to supporting every step of your parenting journey, ensuring your little one gets the very best while giving you peace of mind. Shop with us today and experience the magic of parenthood made easy!
+          hh My Little Ones is your one-stop destination for high-quality baby and mommy essentials, designed to make parenting joyful and stress-free. From cosy baby clothes and safe feeding products to stylish maternity wear and playful toys, we offer a carefully curated selection of trusted items to meet your family’s needs. With a focus on comfort, safety, and affordability, My Little Ones is dedicated to supporting every step of your parenting journey, ensuring your little one gets the very best while giving you peace of mind. Shop with us today and experience the magic of parenthood made easy!
         </p>
       </div>
 
